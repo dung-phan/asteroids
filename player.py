9656648a0353a25a10f4a9ba8c402c15692150a4
dung-phan <dungphan@pm.me>
2025-06-01 19:28:26 +0200

    Moves the triangle around with keyboard

--- player.py
+++ player.py
@@ -1,7 +1,8 @@
 import pygame
+from pygame.transform import rotate
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -22,3 +23,15 @@ class Player(CircleShape):
     # overwrite this from CircleShape
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    # overwrite this from CircleShape
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
