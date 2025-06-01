6df00d0a745a028141a7bc1f381bbc99d54421eb
dung-phan <dungphan@pm.me>
2025-06-01 19:56:22 +0200

    Groups objects by methods

--- main.py
+++ main.py
@@ -7,14 +7,22 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
